3e3523ed63b511c8dc8347e9cd1de0a8176f16fe
Sajmul <sajmul1427@gmail.com>
2024-07-27 11:23:37 +0600

    first commit

---
+++ script/app.js
@@ -0,0 +1,95 @@
+import {getData} from './data.js';
+
+let desc = true;
+
+function init() {
+    const data = getData();
+
+    summery(data);
+    review(data);
+    srtRating(data);
+}
+
+function summery(data) {
+    const flatData = data.flat();
+    const totalProducts = data.length;
+    const totalReview = flatData.length;
+    const totalRating = flatData.reduce((acc,item) => {
+        return acc + item.rating;
+    }, 0);
+    const avgRating = (totalRating/totalReview).toFixed(2);
+
+    const product = document.getElementById('products');
+    product.innerText = totalProducts;
+
+    const review = document.getElementById('review');
+    review.innerText = totalReview;
+
+    const avarage = document.getElementById('avarage');
+    avarage.innerText = avgRating;
+}
+
+function review(data) {
+    const flatData = data.flat();
+    const sorted = flatData.toSorted((a,b) => b.on - a.on);
+    const reviewData = document.querySelector('#review-data ul');
+
+    displayItem(sorted, reviewData);
+
+    
+}
+
+function srtRating(data) {
+    const srtRatingBtn = document.getElementById('sortByRating');
+
+    srtRatingBtn.addEventListener('click',() => {
+        ratingSort(data);
+    })
+}
+
+function ratingSort(data) {
+    const reviewData = document.querySelector('#review-data ul');
+    removeNode(reviewData);
+    const flatData = data.flat();
+
+    desc = !desc;
+
+    const sorting = desc 
+    ? flatData.toSorted((a,b) => b.rating - a.rating)
+    : flatData.toSorted((a,b) => a.rating - b.rating);
+
+    displayItem(sorting, reviewData);
+}
+
+function removeNode(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
+
+function displayItem(info, elem) {
+    info.map((item) => {
+
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        const h3 = document.createElement('h3');
+        const h5 = document.createElement('h5');
+
+
+        li.classList.add('li-style');
+        p.style.marginBottom = '10px';
+
+        h3.innerText = `${item.name} - ${item.rating}`;
+        p.innerText = `${item.review}`;
+        h5.innerText = `${new Intl.DateTimeFormat('en-BD').format(item.on)}`;
+
+        li.appendChild(h3);
+        li.appendChild(p);
+        li.appendChild(h5);
+        elem.appendChild(li);
+    })
+}
+
+init();
---
+++ script/data.js
@@ -0,0 +1,51 @@
+const data = [
+    [
+        {
+            product: 'Fan',
+            name: 'Sajmul',
+            rating: 5.00,
+            review: 'This is very good products',
+            on: 1718892173893
+        },
+        
+        {
+            product: 'Fan',
+            name: 'Sajmul',
+            rating: 5.00,
+            review: 'This is very good products',
+            on: 1718289238293
+        }
+    ],
+
+    [
+        {
+            product: 'Light',
+            name: 'Hossain',
+            rating: 4.78,
+            review: 'What a nice product this is!',
+            on: 17182723838823
+        }
+    ],
+
+    [
+        {
+            product: 'Light',
+            name: 'Hossain',
+            rating: 4.98,
+            review: 'What a nice product this is!',
+            on: 17183892388932
+        },
+
+        {
+            product: 'Light',
+            name: 'Hossain',
+            rating: 4.3,
+            review: 'What a nice product this is!',
+            on: 171829397439
+        }
+    ]
+];
+
+export function getData() {
+    return data;
+}
